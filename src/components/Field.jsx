f42c956eb03459336ef7a330801fba37477b4456
nabeo <nabehiko616@gmail.com>
2021-08-20 13:39:31 +0900

    snake

---
+++ src/components/Field.jsx
@@ -0,0 +1,16 @@
+const Field = ({ fields }) => {
+    return (
+       <div className='field'>
+           {
+              fields.map((row) => {
+                  return row.map((column) => {
+                      return <div className={`dots ${column}`}></div>
+                  })
+
+              })
+           }
+       </div>
+    )
+}
+
+export default Field
